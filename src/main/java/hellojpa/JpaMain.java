2e52851ac50ec56a8ba7eee0829eefa8bd6d88a4
hyobin0726 <hyobin0726@naver.com>
2024-12-18 12:00:05 +0900

    JPQL소개

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -12,11 +13,13 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+//            Member findMember = em.find(Member.class, 1L);
+            //대상이 테이블이 아니고 객체 -
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
 
-            em.persist(member);
             tx.commit();
         }
         catch (Exception e){
